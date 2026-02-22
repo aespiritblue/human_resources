e10b94fccd48a5dbe0461263f9d7de829a7c7555
aespiritblue <163371299+aespiritblue@users.noreply.github.com>
2026-02-21 19:00:07 -0500

    more set up, added tostring to PersonSet

---
+++ PersonList.java
@@ -0,0 +1,5 @@
+public interface PersonList 
+{
+   public abstract void add(Person p); 
+   public abstract Person get(int index);
+}
---
+++ PersonSet.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class PersonSet implements PersonList
+{
+    protected ArrayList<Person> people;
+
+    @Override
+    public void add(Person p) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public Person get(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public String toString()
+    {
+        return " "; //TODO: fill this in
+    }
+    
+}
