c6600451c0647f7061e45bed6832f155558bc419
aespiritblue <163371299+aespiritblue@users.noreply.github.com>
2026-03-02 16:16:03 -0500

    outline for personimperialset

---
+++ PersonImperialSet.java
@@ -0,0 +1,18 @@
+public class PersonImperialSet extends PersonSet
+{
+    @Override
+    public void add(Person p)
+    {
+
+    }
+
+    private double covertHeight(double height)
+    {
+        return 0; //TODO: Fill this in
+    }
+
+    private double convertWeight(double weight)
+    {
+        return 0; //TODO: Fill this in
+    }
+}
---
+++ PersonOrderedSet.java
@@ -0,0 +1,11 @@
+public class PersonOrderedSet extends PersonSet
+{
+
+    @Override
+    public void add(Person p)
+    {
+        //Note to self: use collections.sort
+        //TODO: fill this in
+    }
+
+}
